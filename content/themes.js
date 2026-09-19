dacba3055d91cddf525807f5008b3a31be0e53d7
DTINH <139965711+tinhtinh1908@users.noreply.github.com>
2026-09-20 00:00:09 +0700

    Update theme versions to '1.3-Old'

--- content/themes.js
+++ content/themes.js
@@ -30,7 +30,7 @@ window.CONTENT_THEMES = [
     name: "Light Blue",
     support: "HyperOS 3",
     mode: "Chế độ tối",
-    version: "1.3 - Ngừng cập nhật",
+    version: "1.3-Old",
     art: "blue",
     mark: "L",
 
@@ -53,7 +53,7 @@ window.CONTENT_THEMES = [
     name: "Pink Galaxy",
     support: "HyperOS 3",
     mode: "Chế độ sáng",
-    version: "1.3 - Ngừng cập nhật",
+    version: "1.3-Old",
     art: "pink",
     mark: "P",
 
